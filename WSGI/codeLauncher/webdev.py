5f6e08293e92e661ef05eec9cb6cee62bc2fa175
L.S. <79638053+LuciferStarSS@users.noreply.github.com>
2025-04-15 10:27:53 +0800

    Update webdev.py
    调整多用户支持的部分参数。

--- WSGI/codeLauncher/webdev.py
+++ WSGI/codeLauncher/webdev.py
@@ -1,76 +1,96 @@
 from flask import Flask, request
-from runcode import runcode
 from flask_cors import CORS
+
+from runcode import runcode                 #类RunCode定义：./runcode/runcode.py
 
 app = Flask(__name__)
 
-CORS(app)                                               #ajax跨域支持
+CORS(app)                                   #接口允许跨域访问
 
-@app.route("/runc", methods=['POST'])                   #处理C语言代码，只接受POST请求
+'''
+本程序定义了三个只能接收POST数据的接口：
+    /runc       用于处理提交的C代码
+    /runcpp     用于处理提交的C++代码
+    /runpy      用于处理提交的Python代码
+
+处理过程包括源码的保存、编译和运行，最终返回运行结果/编译错误提示。
+
+所有的处理，都在./runcode/runcode.py里。
+'''
+@app.route("/runc", methods=['POST'])       #处理C语言代码，只接受POST请求
 def runc():
-    #if request.method == 'POST':
-        code = request.form['code']                     #code：表单中提交的C语言源代码
-        stdinData = request.form['data']                #data：表单中提交的程序运行后需要输入的数据
-        stdinArgs = request.form['args']                #data：表单中提交的程序运行后需要输入的数据
-        run = runcode.RunCCode(code, stdinData, stdinArgs) #创建处理C语言的对象
-        resReturnCode,rescompil, resrun = run.run_c_code() #运行C语言源代码
-                                                        #后期考虑是否要添加运行时的命令行参数        
+    postCode = request.form['CODE']         #表单数据：代码
+    postData = request.form['DATA']         #表单数据：用户输入
+    postArgs = request.form['ARGS']         #表单数据：运行参数
+    postUN   = request.form['UN']           #$.post数据：用户名
+    postCID  = request.form['CID']          #$.post数据：班级名称
+    postPN   = request.form['PN']           #$.post数据：项目名称
+    postDD   = request.form['DD']           #$.post数据：日期
         
-        if resrun:                                      #由于数据要以JSON的形式返回给JS，所以要对斜杠和双引号进行转义
-            resrun =resrun.replace("\\","\\\\")
-            resrun =resrun.replace("\"",'\\\"')
-        if rescompil:
-            rescompil =rescompil.replace("\\","\\\\")                        
-            rescompil =rescompil.replace("\"",'\\\"')
+    run = runcode.RunCCode(postCode, postData, postArgs,postUN,postCID,postPN,postDD) #创建处理C语言的对象
+    resReturnCode,rescompil, resrun = run.run_c_code()      #运行C语言源代码
+        
+    if resrun:                                              #由于数据要以JSON的形式返回给JS，所以要对斜杠和双引号进行转义
+        resrun =resrun.replace("\\","\\\\")
+        resrun =resrun.replace("\"",'\\\"')
+    if rescompil:
+        rescompil =rescompil.replace("\\","\\\\")                        
+        rescompil =rescompil.replace("\"",'\\\"')
 
-        if resReturnCode == -99:
-            rescompil="运行超时"
+    if resReturnCode == -99:                                #运行超时时，rescompil无数据，需要单独处理
+        rescompil="运行超时"
             
-        #返回数据为JSON数据格式
-        result="{ \"target\" : \"runc\", \"resrun\" : \""+resrun+"\", \"rescomp\" : \""+rescompil+"\" }"
-        return result
+    result="{ \"target\" : \"runc\", \"resrun\" : \""+resrun+"\", \"rescomp\" : \""+rescompil+"\" }"    #返回数据为JSON数据格式
+    return result
 
-@app.route("/runcpp", methods=['POST'])                #处理C++语言代码
+@app.route("/runcpp", methods=['POST'])     #处理C++语言代码
 def runcpp():
-    #if request.method == 'POST':
-        code = request.form['code']
-        stdinData = request.form['data']
-        stdinArgs = request.form['args']                #data：表单中提交的程序运行后需要输入的数据
-        run = runcode.RunCppCode(code, stdinData, stdinArgs)
-        resReturnCode,rescompil, resrun = run.run_cpp_code()
+    postCode = request.form['CODE']
+    postData = request.form['DATA']
+    postArgs = request.form['ARGS']
+    postUN   = request.form['UN']
+    postCID  = request.form['CID']
+    postPN   = request.form['PN']
+    postDD   = request.form['DD']
         
-        if resrun:
-            resrun =resrun.replace("\\","\\\\")
-            resrun =resrun.replace("\"",'\\\"')
-        if rescompil:
-            rescompil =rescompil.replace("\\","\\\\")                        
-            rescompil =rescompil.replace("\"",'\\\"')
+    run = runcode.RunCppCode(postCode, postData, postArgs,postUN,postCID,postPN,postDD) #创建处理C++语言的对象
+    resReturnCode,rescompil, resrun = run.run_cpp_code()
+        
+    if resrun:
+        resrun =resrun.replace("\\","\\\\")
+        resrun =resrun.replace("\"",'\\\"')
+    if rescompil:
+        rescompil =rescompil.replace("\\","\\\\")                        
+        rescompil =rescompil.replace("\"",'\\\"')
             
-        if resReturnCode == -99:
-            rescompil="运行超时"
+    if resReturnCode == -99:
+        rescompil="运行超时"
             
-        #构建结果输出
-        result="{\"target\":\"runcpp\",\"resrun\":\""+resrun+"\",\"rescomp\":\""+rescompil+"\"}"
-        return result
+    result="{\"target\":\"runcpp\",\"resrun\":\""+resrun+"\",\"rescomp\":\""+rescompil+"\"}"
+    return result
 
-@app.route("/runpy", methods=['POST'])                   #处理Python语言代码
+@app.route("/runpy", methods=['POST'])      #处理Python语言代码
 def runpy():
-    #if request.method == 'POST':
-        code = request.form['code']
-        stdinData = request.form['data']
-        stdinArgs = request.form['args']                #data：表单中提交的程序运行后需要输入的数据
-        run = runcode.RunPyCode(code, stdinData, stdinArgs)
-        rescompil, resrun = run.run_py_code()
+    postCode = request.form['CODE']
+    postData = request.form['DATA']
+    postArgs = request.form['ARGS']
+    postUN   = request.form['UN']
+    postCID  = request.form['CID']
+    postPN   = request.form['PN']
+    postDD   = request.form['DD']
+        
+    run = runcode.RunPyCode(postCode, postData, postArgs,postUN,postCID,postPN,postDD) #创建处理Python语言的对象
+    rescompil, resrun = run.run_py_code()
 
-        if resrun:
-            resrun =resrun.replace("\\","\\\\")
-            resrun =resrun.replace("\"",'\\\"')
-        if rescompil:
-            rescompil =rescompil.replace("\\","\\\\")                        
-            rescompil =rescompil.replace("\"",'\\\"')
+    if resrun:
+        resrun =resrun.replace("\\","\\\\")
+        resrun =resrun.replace("\"",'\\\"')
+    if rescompil:
+        rescompil =rescompil.replace("\\","\\\\")                        
+        rescompil =rescompil.replace("\"",'\\\"')
             
-        result="{\"target\":\"runpy\",\"resrun\":\""+resrun+"\",\"rescomp\":\""+rescompil+"\"}"
-        return result
+    result="{\"target\":\"runpy\",\"resrun\":\""+resrun+"\",\"rescomp\":\""+rescompil+"\"}"
+    return result
 
 if __name__ == "__main__":
     app.run()
