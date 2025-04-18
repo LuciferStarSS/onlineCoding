9e3fb618861eb6a1298beded2ff1d1beb12f8d6c
L.S. <79638053+LuciferStarSS@users.noreply.github.com>
2025-04-18 08:19:10 +0800

    Update index.php
    PHP的json_encode里不能有非UTF-8的中文，而调用gcc以及在windows里读取文件，中文都是非UTF-8的，因此，接收cc.php的返回值，就没有用JSON数据格式。
    此外，为能正确显示编译器显示的含中文的错误信息，只能将C/C++代码保存成ANSI，而Python在处理中文时，又必须是UTF-8的，所以./opt/cc.php里进行了区别对待。

--- just_PHP/index.php
+++ just_PHP/index.php
@@ -50,7 +50,7 @@ if __name__ == "__main__":
 <html>
 <head>
     <title>在线编程系统</title>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8" />
     <link rel="stylesheet" type="text/css" href="./static/devc.css">
     <script src=./static/js/jquery.js></script>
     <script>
@@ -59,36 +59,36 @@ if __name__ == "__main__":
        var gradeid='<?php echo $gradeid;?>';
        var projname='<?php echo $projname;?>';
        var datedir='<?php echo $datedir;?>';
+       var classname='<?php echo $CN;?>';
+
     </script>
 </head>
 <body id="index" class="home">
 <div id="links">
 <ul>
-<li><a href="?t=">C Code</a></li>
-<li><a href="?t=cpp">C++ Code</a></li>
-<li><a href="?t=py">Python Code</a></li>
+<li><a href="?t=">C 语言</a></li>
+<li><a href="?t=cpp">C++ 语言</a></li>
+<li><a href="?t=py">Python 语言</a></li>
 </ul>
+</div>
+<div id="docs" style="position: absolute;top: 14px;left: 276px;">
+<select id=class onchange="getFolders(this)"></select>
+<select id=folders onchange="getFiles(this);"></select>
+<select id=files onchange="getFile(this)"></select>
 </div>
 <div id="content">
     <!-- ************  CODING ZONE  ************ -->
     <form method="post" action="?a=runc">
     <div id="code">
-            <div id="title-code" class="head-section">
-              源代码
-            </div>
-
-            
-<textarea id="text-code" name="code" rows=15 cols=60>
-<?php
+        <div id="title-code" class="head-section">源代码</div>
+        <textarea id="text-code" name="code" rows=15 cols=60><?php
 echo htmlspecialchars($demo_source[$type]);
 ?>
-</textarea><br/>
-<div id="text-code-ace" class="hidden"><?php
+        </textarea><br/>
+        <div id="text-code-ace" class="hidden"><?php
 echo htmlspecialchars($demo_source[$type]);
 ?> 
-</div>
-
-
+        </div>
     </div>
     
     <!-- ************ RUNNING ZONE RESULTS ************ -->
@@ -96,7 +96,7 @@ echo htmlspecialchars($demo_source[$type]);
     <div id="result">
         <div style="position: relative;top: 0px;height: 5%;font-weight: bold;">命令行参数：<input style="position: absolute;left: 90px;width: 270px;" id="inputargs" class="head-section" /></div>
         <div style="position: relative;top: 6px;height: 6%;font-weight: bold;">模拟输入：<textarea  style="position: absolute;left: 90px;width: 272px;" id="inputdata" class="head-section" /></textarea></div>
-        <input style="position: absolute;right: 121px;left: 380px;top: 19px;height: 50px;width: 60px;;"id="launch-button" class="head-section" type=button onclick="goLaunching()" value="运行" />
+        <input style="position: absolute;right: 121px;left: 380px;top: 19px;height: 50px;width: 60px;;"id="launch-button" class="head-section" type=button onclick="goLaunching()" value="运行" title="保存并运行"/>
         <div style="position: relative;top: 19px;height: 2%;left:0px" id="title-result" class="head-section">运行结果</div>
         <textarea style=" height: 69%;   position: relative;top: 42px;" id="text-result" rows="18" cols="70" readonly>无输出</textarea>
     </div>
@@ -123,7 +123,7 @@ echo htmlspecialchars($demo_source[$type]);
     // Set up the editor
     var editor = ace.edit("text-code-ace");
     editor.setTheme("ace/theme/tomorrow");
-    var language = ("runc" === "runpy") ? "python" : "c_cpp";
+    var language = ("<?php echo $type;?>" === "py") ? "python" : "c_cpp";
     editor.getSession().setMode("ace/mode/" + language);
 
     // Make sure we copy the content of the editor to the textarea before posting
@@ -136,31 +136,102 @@ echo htmlspecialchars($demo_source[$type]);
 <script>
 function goLaunching()
 {
-   var username='<?php echo $username;?>';
-   var classid='<?php echo $classid;?>';
-   var classname='<?php echo $CN;?>';
-   var gradeid='<?php echo $gradeid;?>';
-   var projname='<?php echo $projname;?>';
-   var datedir='<?php echo $datedir;?>';
-   var data=$("#inputdata")[0].value;
-   var args=$("#inputargs")[0].value;
+   var data=document.getElementById("inputdata").value;
+   var args=document.getElementById("inputargs").value;
 
-   $("#text-result").text("");
-   $("#text-compile").text("");
+   document.getElementById("text-result").value="";
+   document.getElementById("text-compile").value="";
+
    
-   $.post("cc.php?t=<?php echo $type;?>", {"UN":username,"CID":classname,"PN":projname, "DD":datedir, "CODE": editor.getValue(),"DATA":data, "ARGS":args}, function (data) {
+   $.post("./opt/cc.php?t=<?php echo $type;?>", {"UN":username,"CN":classname,"PN":projname, "DD":datedir, "CODE": editor.getValue(),"DATA":data, "ARGS":args}, function (data) {
+      var dataArr=data.split("<+-NOJSON-+>");
+      if(dataArr.length==3)
+      {
+         document.getElementById("text-result").value=dataArr[1];
+         document.getElementById("text-compile").value=dataArr[2];
+      }
+      //   document.getElementById("text-result").value=data;
+return;
       data=data.replaceAll("\r","\\r");
       data=data.replaceAll("\n","\\n");
       var jsonData=JSON.parse(data);
       if(jsonData)
       {
-         $("#text-result").text(jsonData.resrun);
-
-         $("#text-compile").text(jsonData.rescomp);
+         //document.getElementById("text-result").value=atob(jsonData.resrun);
+         //document.getElementById("text-compile").value=atob(jsonData.rescomp);
+         document.getElementById("text-result").value=jsonData.resrun;
+         document.getElementById("text-compile").value=jsonData.rescomp;
       }
-
    });
 }
+
+
+function getClass()
+{
+   document.getElementById("class").append(new Option("请选择班级",""));
+   document.getElementById("class").append(new Option(classname,classname));
+
+}
+
+function getFolders(o)
+{
+   if(o.value){
+      classname=o.value;
+      $.post("./opt/getFolders.php", {"C":classid, "CN":o.value}, function (data) {
+         var jsonData=JSON.parse(data);
+         if(jsonData)
+         {
+            var folders=document.getElementById("folders");
+            folders.length=0;
+            folders.append(new Option("请选择日期",""));
+
+            var arrKeys=Object.keys(jsonData);
+            for(i=0;i<arrKeys.length;i++)
+            {
+               folders.append(new Option(jsonData[arrKeys[i]],jsonData[arrKeys[i]]));
+            }
+         }
+      });
+   }
+}
+
+function getFiles(o)
+{
+   if(o.value){
+      datedir=o.value;
+      $.post("./opt/getFiles.php?T=<?php echo $type;?>", {"UN":username,"C":classid, "CN":classname, "DD":o.value}, function (data) {
+         var jsonData=JSON.parse(data);
+         if(jsonData)
+         {
+             var files=document.getElementById("files");
+             files.length=0;
+             files.append(new Option("请选择文件",""));
+
+             var arrKeys=Object.keys(jsonData);
+             for(i=0;i<arrKeys.length;i++)
+             {
+                files.append(new Option(jsonData[arrKeys[i]],jsonData[arrKeys[i]]));
+             }
+         }
+      });
+   }
+}
+
+function getFile(o)
+{
+   if(o.value){
+      $.post("./opt/getFile.php?T=<?php echo $type;?>", {"UN":username,"C":classid, "CN":classname, "DD":datedir, "FN":o.value}, function (data) {
+         //var jsonData=JSON.parse(data);
+         if(data)
+         {
+            editor.setValue(data);
+         }
+      });
+   }
+}
+
+getClass();
+
 </script>
 </body>
 </html>
